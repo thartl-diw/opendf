2f09cc619be57d8a1c1623d09a112633c1e11daa
thartl-diw <160223397+thartl-diw@users.noreply.github.com>
2024-02-26 09:14:10 +0100

    Delete exec_csv2xml.py

--- exec_csv2xml.py
+++
@@ -1,9 +0,0 @@
-def exec_csv2xml(input_dir, output_dir, ):
-    from sfi import Macro
-    import os
-    import csv2xml
-    temp_dir = os.environ["TEMP"]
-    csv2xml.variables_arg = Macro.getLocal('variables_arg')
-    csv2xml.export_data = Macro.getLocal('export_data')
-    csv2xml.csv2xml(input_dir, output_dir)
-end
